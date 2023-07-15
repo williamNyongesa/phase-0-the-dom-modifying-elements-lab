512a4a0450c31dd95248d5c688a66b5150c82ac0
williamNyongesa <marakawilliam@gmail.com>
2023-07-15 18:29:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let main=document.getElementById("main");
+main.remove();
+
+let newHeader= document.createElement("h1");
+newHeader.setAttribute( "id", "victory");
+
+newHeader.textContent = ('YOUR-NAME is the champion');
+document.body.append(newHeader);
